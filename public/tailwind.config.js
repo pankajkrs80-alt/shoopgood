d5e0bc590b4d6bed3c87617b1ce7cafe39baf03f
Your NameKartik <pankajkr80@gmail.com>
2026-08-05 22:35:17 +0530

    done

---
+++ public/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // This tells Tailwind to look at your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
